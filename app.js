667cd5b16dbb2dce457db72a737d97c421eed827
Vaibhav Rathore <vaibhavrathorema@gmail.com>
2022-12-04 12:41:44 +0530

    add backend file

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cookieParser());
+
+
+//for get/post data in json formate
+app.use(express.json())
+
+
+
+//getting Secrat key  and mongodb key from config . env
+dotenv.config({ path: "./config.env" });
+app.use(require("./Routes/router.js"));
+require("./DB/conn");
+
+
+// if(process.env.NODE_ENV == 'production'){
+//     app.use(express.static("client/build"))
+// }
+
+
+//run server
+app.listen(PORT,()=>{
+    console.log('server is running on ' + PORT)
+})
